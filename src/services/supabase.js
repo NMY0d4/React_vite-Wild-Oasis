8e6778b08cbdb96b8af48fddc34647733abe06a0
Grégory Marini <greg.marini@hotmail.fr>
2023-08-08 11:55:59 +0200

    configure and connect supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://gfbeuvqxvnkjggwkcsmr.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmYmV1dnF4dm5ramdnd2tjc21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE0ODI2MzcsImV4cCI6MjAwNzA1ODYzN30.8JZVgNqu0y_xLxL6GN3h3PtDbGloxHCSF2M4fWbWnt8';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
