ce7b820f88f42dcd86a7d42eaace1ed2da4f1338
Grégory Marini <greg.marini@hotmail.fr>
2023-08-11 15:42:16 +0200

    create modal for createEditCabin form

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
 import CreateCabinForm from './CreateCabinForm';
+import Modal from '../../ui/Modal';
 
 function AddCabin() {
-  const [showForm, setShowForm] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   return (
     <div>
-      <Button onClick={() => setShowForm((show) => !show)}>
+      <Button onClick={() => setIsOpenModal((show) => !show)}>
         Add new cabin
       </Button>
-      {showForm && <CreateCabinForm onShowForm={setShowForm} />}
+      {isOpenModal && (
+        <Modal onClose={() => setIsOpenModal(false)}>
+          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+        </Modal>
+      )}
     </div>
   );
 }
--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -10,7 +10,7 @@ import { useForm } from 'react-hook-form';
 import { useCreateCabin } from './useCreateCabin';
 import { useEditCabin } from './useEditCabin';
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { id: editId, ...editValues } = cabinToEdit;
   const isEditSession = Boolean(editId);
   const { register, handleSubmit, reset, getValues, formState } = useForm({
@@ -29,6 +29,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     const onSuccessOptions = {
       onSuccess: () => {
         reset();
+        onCloseModal?.();
       },
     };
 
@@ -45,7 +46,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   // }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit /* , onError*/)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit /* , onError*/)}
+      type={onCloseModal ? 'modal' : 'regular'}
+    >
       <FormRow label='Cabine Name' error={errors?.name?.message || null}>
         <Input
           type='text'
@@ -130,7 +134,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation='secondary' type='reset'>
+        <Button
+          variation='secondary'
+          type='reset'
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isWorking}>
