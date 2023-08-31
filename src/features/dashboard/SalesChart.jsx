c4a831cdc5ec85d6dfa440d6c1e7923bb8f1ae17
Grégory Marini <greg.marini@hotmail.fr>
2023-08-31 18:28:37 +0200

    styling SalesChart component

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 import DashboardBox from './DashboardBox';
 import Heading from '../../ui/Heading';
-import { Area, AreaChart, CartesianGrid, Tooltip } from 'recharts';
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import { useDarkMode } from '../../context/DarkModeContext';
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -45,30 +54,50 @@ const fakeData = [
   { label: 'Feb 06', totalSales: 1450, extrasSales: 400 },
 ];
 
-const isDarkMode = true;
-const colors = isDarkMode
-  ? {
-      totalSales: { stroke: '#4f46e5', fill: '#4f46e5' },
-      extrasSales: { stroke: '#22c55e', fill: '#22c55e' },
-      text: '#e5e7eb',
-      background: '#18212f',
-    }
-  : {
-      totalSales: { stroke: '#4f46e5', fill: '#c7d2fe' },
-      extrasSales: { stroke: '#16a34a', fill: '#dcfce7' },
-      text: '#374151',
-      background: '#fff',
-    };
-
 function SalesChart() {
+  const { isDarkMode } = useDarkMode();
+
+  const colors = isDarkMode
+    ? {
+        totalSales: { stroke: '#4f46e5', fill: '#4f46e5' },
+        extrasSales: { stroke: '#22c55e', fill: '#22c55e' },
+        text: '#e5e7eb',
+        background: '#18212f',
+      }
+    : {
+        totalSales: { stroke: '#4f46e5', fill: '#c7d2fe' },
+        extrasSales: { stroke: '#16a34a', fill: '#dcfce7' },
+        text: '#374151',
+        background: '#fff',
+      };
   return (
     <StyledSalesChart>
       <Heading as='h2'>Sales</Heading>
-      <AreaChart data={fakeData} height={300} width={700}>
-        <CartesianGrid />
-        <Tooltip />
-        <Area dataKey='totalSales' type='monotone' stroke='red' fill='orange' />
-      </AreaChart>
+      <ResponsiveContainer height={300} width='100%'>
+        <AreaChart data={fakeData}>
+          <XAxis
+            dataKey={'label'}
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+          <YAxis
+            unit='$'
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+          <CartesianGrid
+            strokeDasharray='4'
+            contentStyle={{ backgroundColor: colors.background }}
+          />
+          <Tooltip />
+          <Area
+            dataKey='totalSales'
+            type='monotone'
+            stroke={colors.totalSales.stroke}
+            fill={colors.totalSales.fill}
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </StyledSalesChart>
   );
 }
