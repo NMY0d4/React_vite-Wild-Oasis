19e17251383bc8388f03ec6535099b869da009df
Grégory Marini <greg.marini@hotmail.fr>
2023-08-31 18:33:28 +0200

    add second Area in AreaChart for extrasSales

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -85,16 +85,25 @@ function SalesChart() {
             tick={{ fill: colors.text }}
             tickLine={{ stroke: colors.text }}
           />
-          <CartesianGrid
-            strokeDasharray='4'
-            contentStyle={{ backgroundColor: colors.background }}
-          />
-          <Tooltip />
+          <CartesianGrid strokeDasharray='4' />
+          <Tooltip contentStyle={{ backgroundColor: colors.background }} />
           <Area
             dataKey='totalSales'
             type='monotone'
             stroke={colors.totalSales.stroke}
             fill={colors.totalSales.fill}
+            strokeWidth={2}
+            name='Total Sales'
+            unit='$'
+          />
+          <Area
+            dataKey='extrasSales'
+            type='monotone'
+            stroke={colors.extrasSales.stroke}
+            fill={colors.extrasSales.fill}
+            strokeWidth={2}
+            name='Extras Sales'
+            unit='$'
           />
         </AreaChart>
       </ResponsiveContainer>
